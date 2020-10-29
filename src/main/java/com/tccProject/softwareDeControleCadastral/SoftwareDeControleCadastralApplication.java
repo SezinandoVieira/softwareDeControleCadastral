7d9451e1c11c522651d61afb2f9410fc5fa3c454
SezinandoVieira <sezinado.vieira@gmail.com>
2020-10-29 16:03:45 -0300

    Upando a base para o começo da fabricação do software

---
+++ src/main/java/com/tccProject/softwareDeControleCadastral/SoftwareDeControleCadastralApplication.java
@@ -0,0 +1,13 @@
+package com.tccProject.softwareDeControleCadastral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDeControleCadastralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDeControleCadastralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tccProject/softwareDeControleCadastral/SoftwareDeControleCadastralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tccProject.softwareDeControleCadastral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDeControleCadastralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
